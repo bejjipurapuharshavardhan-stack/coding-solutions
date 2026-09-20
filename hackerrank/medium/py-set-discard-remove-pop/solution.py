8f574ed02727edef73d3500f5e0fbd1c404309f6
Harsha Vardhan Bejjipurapu <bejjipurapuharshavardhan@gmail.com>
2026-09-20 13:07:35 +0530

    ✅ Set .discard(), .remove() & .pop() | Medium | Python | Runtime: N/A | Memory: N/A | 2026-09-20

---
+++ hackerrank/medium/py-set-discard-remove-pop/solution.py
@@ -0,0 +1,15 @@
+n = int(input())
+s = set(map(int, input().split()))
+num_commands = int(input())
+
+for _ in range(num_commands):
+    cmd = input().split()
+    
+    if cmd[0] == 'pop':
+        s.pop()
+    elif cmd[0] == 'remove':
+        s.remove(int(cmd[1]))
+    elif cmd[0] == 'discard':
+        s.discard(int(cmd[1]))
+
+print(sum(s))
