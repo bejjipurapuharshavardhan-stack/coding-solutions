db5dfd15948aa0f10004cff730b9f10f44ef9244
Harsha Vardhan Bejjipurapu <bejjipurapuharshavardhan@gmail.com>
2026-09-24 12:35:18 +0530

    ✅ Set .symmetric_difference() Operation | Medium | Python | Runtime: N/A | Memory: N/A | 2026-09-24

---
+++ hackerrank/medium/py-set-symmetric-difference-operation/solution.py
@@ -0,0 +1,7 @@
+num_student1 = int(input())
+set1 = set(map(int, input().split()))
+num_student2 = int(input())
+set2 = set(map(int, input().split()))
+
+res = set1.symmetric_difference(set2)
+print(len(res))
