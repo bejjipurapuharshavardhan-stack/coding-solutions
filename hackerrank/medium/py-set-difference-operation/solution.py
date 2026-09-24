3b5089028ef223ff55b11d6041cfd2a9981fd5c1
Harsha Vardhan Bejjipurapu <bejjipurapuharshavardhan@gmail.com>
2026-09-24 12:34:09 +0530

    ✅ Set .difference() Operation | Medium | Python | Runtime: N/A | Memory: N/A | 2026-09-24

---
+++ hackerrank/medium/py-set-difference-operation/solution.py
@@ -0,0 +1,7 @@
+student_num1 = int(input())
+set1 = set(map(int, input().split()))
+student_num2 = int(input())
+set2 = set(map(int, input().split()))
+
+res = set1.difference(set2)
+print(len(res))
